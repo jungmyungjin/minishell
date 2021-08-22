9df01a1fd1104794ac4db36d651990b76220d8e7
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-08-23 00:56:55 +0900

    #7 ft_cd, ft_pwd 간단한 명령어 함수 추가 (#8)
    * Makefile 추가
    
    * Makefile 불필요한 주석 제거
    
    * #7 ft_cd, ft_pwd 간단한 명령어 함수 추가
    
    * CMakefile 파일 경로 수정

---
+++ src/built-in/ft_cd.c
@@ -0,0 +1,16 @@
+/*
+ * cd 명령은 단순히 타겟 경로에 이동하는 명령
+ * 멘데토리 - 다른 옵션 X, 오직 상대경로/절대경로 이동
+ *
+ * chdir() 함수 자체가 상대경로도 인식해주기 때문에 따로 처리가 필요하지 않음
+ */
+
+void	ft_cd()
+{
+	char	*target_path;
+	int	rtn;
+
+	rtn = chdir( target_path );	// 경로 이동
+	if(rtn == -1)	// 에러 케이스
+		printf("%s\n", strerror(errno));	// 에러 출력
+}
