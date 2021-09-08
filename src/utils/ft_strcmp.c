c0d20aa5a9f9a3bbd9fe45adfdc5ef7608f59813
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-09-09 02:13:03 +0900

    추가: Libft strcmp 함수 에러처리 추가

--- src/utils/ft_strcmp.c
+++ src/utils/ft_strcmp.c
@@ -2,6 +2,12 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	else if (s1 == NULL)
+		return (ft_strlen(s2) * -1);
+	else if (s2 == NULL)
+		return (ft_strlen(s1));
     while (*s1 != '\0' && *s2 != '\0')
     {
         if (*s1 != *s2)
