f88947981d12f5b4eba44e0097df54f918864cbf
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-09-19 00:51:11 +0900

    빌트인 echo에 -n 옵션 추가

--- src/built-in/ft_echo.c
+++ src/built-in/ft_echo.c
@@ -12,14 +12,23 @@ void ft_echo(t_simple_cmd *simple_cmd, t_list *env)
 {
 	char *contents;
 	int idx;
+	int used_newline;
 
 	idx = 0;
+	used_newline = 1;
 	contents = NULL;
+	if (ft_strcmp(simple_cmd->argv[1], "-n") == 0)
+	{
+		used_newline = 0;
+		idx++;
+	}
 	while(simple_cmd->argv[++idx])
 	{
 		contents = ft_strjoin(contents, ft_strdup(simple_cmd->argv[idx]));
 		contents = ft_strjoin(contents, ft_strdup(" "));
 	}
 	if (contents)
-		printf("%s\n", contents);
+		write(1, contents, ft_strlen(contents));
+	if (used_newline)
+		write(1, "\n", 1);
 }
