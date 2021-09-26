f2d32f81b6c41c6ff5db612ad2a5fa0582480647
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-09-27 05:57:49 +0900

    built-in : exit 함수 인자값 동작 추가 (#44)

--- src/built-in/ft_exit.c
+++ src/built-in/ft_exit.c
@@ -11,5 +11,25 @@
 
 void ft_exit(t_simple_cmd *simple_cmd, t_list *env, t_mcb *mcb)
 {
-	exit(global.rtn);
+	int exit_code;
+
+	ft_putendl_fd("exit", STDOUT_FILENO);
+	if (simple_cmd->argv[1] == NULL)
+		exit(global.rtn);
+	exit_code = ft_atoi(simple_cmd->argv[1]);
+	if (ft_strlen(simple_cmd->argv[1]) > 3 || exit_code < 0 || 255 < exit_code)
+	{
+		ft_putstr_fd(SHELL_NAME, STDERR_FILENO);
+		ft_putendl_fd(" : exit: This is not a valid numerical range", STDERR_FILENO);
+		exit(255);
+	}
+	if (simple_cmd->argv[2] != NULL)
+	{
+		ft_putstr_fd(SHELL_NAME, STDERR_FILENO);
+		ft_putendl_fd(" : exit: too many arguments", STDERR_FILENO);
+		global.rtn = 1;
+		return;
+	}
+	else
+		exit(exit_code);
 }
